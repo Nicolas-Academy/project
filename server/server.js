9db2f0d04399109997fa6f28551b4059e210c177
Nicolas-Academy <rocacastedonicolas@gmail.com>
2024-11-17 15:23:10 +1200

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 require('dotenv').config(); 
 const express = require("express");
 const cors = require("cors");
-const connectToDb = require('./config/connectToDb');
+const connectToDb = require("./connectTODb"); 
 
 
 // Importar módulos de rutas.
